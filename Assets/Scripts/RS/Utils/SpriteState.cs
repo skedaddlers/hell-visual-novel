690c4f28146bbeb4f56c7ed1f2d7d0d64f740fcf
Roni <rony.se7@gmail.com>
2025-07-14 05:45:28 +0700

    adding utils

---
+++ Assets/Scripts/RS/Utils/SpriteState.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SpriteState : MonoBehaviour
+{
+    [SerializeField]
+    protected int _state = 0;
+
+    [SerializeField] protected UnityEvent onStageChanged;
+
+    public virtual int State
+    {
+        get
+        {
+            return _state;
+        }
+        set
+        {
+            if (_state != value) {
+                onStageChanged?.Invoke();
+            }
+            _state = value;
+            RefreshUI();
+        }
+    }
+
+    public int StateCount
+    {
+        get
+        {
+            return sprites.Count;
+        }
+    }
+
+    public List<Sprite> sprites;
+
+    public SpriteRenderer spriteRenderer;
+    public Image image;
+
+    private void OnEnable()
+    {
+        RefreshUI();
+    }
+
+    private void OnValidate()
+    {
+        RefreshUI();
+    }
+    public void RefreshUI()
+    {
+        var s = Mathf.Clamp(_state, 0, sprites.Count - 1);
+        var sprite = sprites.Count > 0 ? sprites[s] : null;
+
+        if (spriteRenderer)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+
+        if (image)
+        {
+            image.sprite = sprite;
+        }
+    }
+
+    public void SetLast()
+    {
+        State = StateCount - 1;
+    }
+
+    public void SetFirst()
+    {
+        State = 0;
+    }
+
+    public void SetLast(bool value) {
+        if (value) {
+            SetLast();
+            return;
+        } 
+        SetFirst();
+    }
+    
+    public void SetSprites(Sprite[] sprites)
+    {
+        this.sprites.Clear();
+        this.sprites.AddRange(sprites);
+        RefreshUI();
+    }
+}
